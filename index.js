d8d9ebf6de6d4b676161ade47fa0d06fbac2d96d
sofia Luna <Sofia21luna@gmail.com>
2020-04-22 23:06:40 -0400

    get model year works!

--- index.js
+++ index.js
@@ -259,8 +259,13 @@ function getLastCarInfo(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  let modelYears = [];
+  for(let i = 0; i < inventory.length; i++){
+    modelYears[i] = inventory[i].car_year
+  }
+
+  return modelYears;
 }
 
 /**
